fe1f49615735aef39aed81c8f7cf0d44dcff2a4c
Danny Harding <dannyharding10@gmail.com>
2017-03-28 19:10:20 -0600

    the beginning

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index.html', { root: 'public' });
+});
+
+module.exports = router;
+
+module.exports = function(io) {
+	var app = require('express');
+	var router = app.Router();
+
+	io.on('connection', function(socket) {
+		console.log('user connected');
+	});
+
+	return router;
+}
