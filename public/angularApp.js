35807d6ae557d22b4806d47309a53a064a18655d
Danny Harding <dannyharding10@gmail.com>
2017-03-30 15:06:42 -0600

    more cleanup

--- public/angularApp.js
+++ public/angularApp.js
@@ -22,11 +22,12 @@ angular.module('reminder', [])
   });
 
   socket.on('add reminder response', function(response) {
-  	if (response == 'success') {
-  		$scope.notice = 'Reminder added!';
-  	} else {
-  		$scope.notice = response;
+  	if (response.success) {
+	    $scope.newReminder.todo = '';
+	    $scope.newReminder.time = '';
   	}
+
+  	$scope.notice = response.message;
   });
 
 
@@ -36,7 +37,7 @@ angular.module('reminder', [])
   $scope.notice = '';
 
 //I don't think we need this
-  Date.prototype.yyyymmdd = function() {
+ /* Date.prototype.yyyymmdd = function() {
     var mm = this.getMonth() + 1; // getMonth() is zero-based
     var dd = this.getDate();
     var hh = this.getHours();
@@ -49,49 +50,15 @@ angular.module('reminder', [])
            [(hh>9 ? '' : '0') + hh,
             (mi>9 ? '' : '0') + mi,
             (ss>9 ? '' : '0') + ss].join(':');
-  };
+  };*/
 
   $scope.login = function(username) {
   	socket.emit('login', username);
   }
-
+  socket.emit('login', 'Danny');
   $scope.addReminder = function (reminder) {
-    socket.emit('add reminder', {
-    	user: reminder.username,
-    	text: reminder.todo,
-    	time: dateAdd(new Date(), reminder.timeUnit, reminder.time).toISOString()
-    });
-
-    reminder.todo = '';
-    reminder.time = '';
+  	//remove username from reminder in html
+    socket.emit('add reminder', reminder);
   };
-
-  /**
-  * Adds time to a date. Modelled after MySQL DATE_ADD function.
-  * Example: dateAdd(new Date(), 'minute', 30)  //returns 30 minutes from now.
-  *
-  * date  Date to start with
-  * interval  One of: year, quarter, month, week, day, hour, minute, second
-  * units  Number of units of the given interval to add.
-  */
-  function dateAdd(date, interval, units){
-    var ret = new Date(date); //don't change original date
-      var checkRollover = function() { if(ret.getDate() != date.getDate()) ret.setDate(0);};
-      switch(interval.toLowerCase()) {
-      case 'year'   :  ret.setFullYear(ret.getFullYear() + units); checkRollover();  break;
-      case 'quarter':  ret.setMonth(ret.getMonth() + 3*units); checkRollover();  break;
-      case 'month'  :  ret.setMonth(ret.getMonth() + units); checkRollover();  break;
-      case 'week'   :  ret.setDate(ret.getDate() + 7*units);  break;
-      case 'day'    :  ret.setDate(ret.getDate() + units);  break;
-      case 'hour'   :  ret.setTime(ret.getTime() + units*3600000);  break;
-      case 'minute' :  ret.setTime(ret.getTime() + units*60000);  break;
-      case 'second' :  ret.setTime(ret.getTime() + units*1000);  break;
-      default       :  ret = undefined;  break;
-    }
-    return ret;
-  }
-
-
-
 
 }]);
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,31 @@ router.get('/', function(req, res, next) {
   res.render('index.html', { root: 'public' });
 });
 
+/**
+* Adds time to a date. Modelled after MySQL DATE_ADD function.
+* Example: dateAdd(new Date(), 'minute', 30)  //returns 30 minutes from now.
+*
+* date  Date to start with
+* interval  One of: year, quarter, month, week, day, hour, minute, second
+* units  Number of units of the given interval to add.
+*/
+function dateAdd(date, interval, units){
+var ret = new Date(date); //don't change original date
+  var checkRollover = function() { if(ret.getDate() != date.getDate()) ret.setDate(0);};
+  switch(interval.toLowerCase()) {
+  case 'year'   :  ret.setFullYear(ret.getFullYear() + units); checkRollover();  break;
+  case 'quarter':  ret.setMonth(ret.getMonth() + 3*units); checkRollover();  break;
+  case 'month'  :  ret.setMonth(ret.getMonth() + units); checkRollover();  break;
+  case 'week'   :  ret.setDate(ret.getDate() + 7*units);  break;
+  case 'day'    :  ret.setDate(ret.getDate() + units);  break;
+  case 'hour'   :  ret.setTime(ret.getTime() + units*3600000);  break;
+  case 'minute' :  ret.setTime(ret.getTime() + units*60000);  break;
+  case 'second' :  ret.setTime(ret.getTime() + units*1000);  break;
+  default       :  ret = undefined;  break;
+}
+return ret;
+}
+
 module.exports = function(io) {
 	io.on('connection', function(socket) {
 		// socket.username = 'Danny';
@@ -18,11 +43,21 @@ module.exports = function(io) {
 		});
 
 		socket.on('add reminder', function(reminder) {
+			var response = {};
 			if (socket.username) {
-				mongo.addReminder(reminder);
-				var response = 'success';
+
+				var mongoReminder = {
+					user: socket.username,
+					text: reminder.todo,
+					time: dateAdd(new Date(), reminder.timeUnit, reminder.time).toISOString()
+				}
+
+				mongo.addReminder(mongoReminder);
+				response.success = true;
+				response.message = 'Reminder created for ' + socket.username;
 			} else {
-				var response = 'must be signed in to add reminder';
+				response.success = false;
+				response.message = 'Must be signed in to add reminder';
 			}
 			socket.emit('add reminder response', response);
 		});
